cf6a3f67227658b32397974ff027aa618835bd67
Abhijeet Pandey <156208339+abhijeetpandey0008@users.noreply.github.com>
2026-03-09 18:00:07 +0530

    Update llm.py

--- utils/llm.py
+++ utils/llm.py
@@ -1,3 +1,26 @@
+# import os
+# from dotenv import load_dotenv
+# from huggingface_hub import InferenceClient
+
+# load_dotenv()
+
+# client = InferenceClient(
+#     token=os.getenv("HUGGINGFACEHUB_API_TOKEN")
+# )
+
+
+# def generate_response(prompt):
+
+#     response = client.text_generation(
+#         model="meta-llama/Llama-3-8B-Instruct",
+#         messages=[
+#             {"role": "user", "content": prompt}
+#         ],
+#         max_tokens=512,
+#         temperature=0.2
+#     )
+#     return response.choices[0].message.content
+
 import os
 from dotenv import load_dotenv
 from huggingface_hub import InferenceClient
@@ -5,21 +28,18 @@ from huggingface_hub import InferenceClient
 load_dotenv()
 
 client = InferenceClient(
+    model="meta-llama/Meta-Llama-3-8B-Instruct",
     token=os.getenv("HUGGINGFACEHUB_API_TOKEN")
 )
-
 
 def generate_response(prompt):
 
     response = client.text_generation(
-        model="meta-llama/Llama-3-8B-Instruct",
-        messages=[
-            {"role": "user", "content": prompt}
-        ],
-        max_tokens=512,
+        prompt=prompt,
+        max_new_tokens=512,
         temperature=0.2
     )
 
+    return response
 
-    return response.choices[0].message.content
 
